38b129a804e29d06b0e9ef16d5e3845517abf1ab
mdayubali <ayubh9288@gmail.com>
2023-03-20 11:50:33 +0600

    Python basics

---
+++ example.py
@@ -0,0 +1,8 @@
+snake_case = "My Name is Md Ayub Ali"
+# print(snake_case.capitalize())
+# print(snake_case.upper())
+# spliting in python
+#print(snake_case.split()) #by default
+# print(snake_case.split('a'))
+# split using slice notaion (start:end) for fix amount of index in a string 
+print(snake_case[0:7])
---
+++ fstringliteral.py
@@ -0,0 +1,6 @@
+first_name = "Md Ayub"
+last_name = "Ali"
+a = 2
+# If you want print a variable value inside any hard code the use the f before the string and use the varibale inside the curly brack
+print(f"My name is {first_name} {last_name}")
+print(f"Last name is {last_name}")
