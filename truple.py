8effa97092d8e9646e0f27948ca2b8e257aa2dde
mdayubali <ayubh9288@gmail.com>
2023-03-25 08:49:46 +0600

    Truple like as list

---
+++ truple.py
@@ -0,0 +1,5 @@
+# truple is same as list but it's start with perenthesis bracket() where in list use squre[] brackets 
+# truple is immutable where list muteable
+
+my_truple = (1,2,4,5)
+print(my_truple[0])
